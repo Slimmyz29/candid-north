bba5c7bf007445a1255259897acb3f77d7c3855d
Slimmyz29 <105140312+Slimmyz29@users.noreply.github.com>
2023-04-09 22:07:41 +0200

    Add files via upload

---
+++ public/js/transition_page.js
@@ -0,0 +1,151 @@
+content_page1 = [
+    `<h3><strong>Azrael XXX</strong></h3><img src="public/img/Apparence.jpg" style="width: auto;height: 75%;" alt="">`,
+    `
+    <h3><strong>Chapitre 1:l’enfance</strong></h3>
+        <p>Azrael durant son enfance était un enfant très sain et attentionné jusqu'à ce que le désastre arrive son propre frère a décidé d’essayer de le tuer durant la nuit,ce dernier apercevant le couteau dans la main de sont grand frere, se mit a se lever doucement tout en prenant le couteau arborant un ruby a sa garde que sa mere lui avait offert au part avant,  tout a coup sans meme essayer de discuter il posa son pied droit sur le mur pour prendre une impulsion et gagner en vitesse tout en jetant son couteau en l’air et le faisant changer de position fesant en sorte qu’il soit dans une bonne position puis sur rua sur son grand frère en le désarmant avec un coup de pied dans la main puis planta le couteau droit dans son ventre…
+        le regard vide couvert de sang,Azrael sortit de sa maison pour rendre une petite visite de courtoisie à son père ,ancien sergent de la brigade special qui les avaient jetter comme des ordures dans les bas fond,une fois dans cet endroit “magnifique”. 
+        </p>
+        <button onclick="PageSuivante(1)">Page Précedente</button></div>
+    `,
+    `
+    <p>“Que fais-tu !” demanda- t’il avec une intonation prononcée a sont amis.
+    “Lâche-moi, ce ne sont pas tes affaires” répliqua-t-il en le repoussant.
+    
+    “Abruti, fait donc ce qu’il te plaira, mais le chemin que tu t’apprêtes a emprunté te mènera vers une mort certaine”répondit-il en levant les mains, signe d’abandon face à cette tête de mule
+    
+    “Parfait, alors laisse-moi” dit il en s'éloignent.
+    
+    Quand soudain un 3e personne interrompu Azrael dans sa course.
+    
+    “Tu devrais écouter ton ami, vermine, ta vie ne vaut rien à mes yeux, il serait inutile de la gâcher ainsi à un si jeune âge n'est-ce pas ?” dit l’homme qui arborait la licorne sur son blason  
+    </p>
+    <h3><strong>Chapitre 3: Abarnikov</strong></h3>
+    <p>“T…es..tes qui toi” dit-il en tremblant</p>
+    <p>“je suis l’homme qui peut mettre fin à tes jours Azrael” dit-il en jetant un rubis sur Azrael en lui fessant signe de decampé.</p>
+    <p>“E..eu…euuuh comment tu connais mon nom, et pourquoi t’avait mon Ruby.” dit-il</p>
+    <p>“tu poses trop de question pour la vermine que tu es et tu vas m’écouter attentivement, si tu veux faire le mal fait le bien, car ta un ennemi bien plus puissant qu’un titan alors fait attention à toi” lui répondit-il en le braquant avec son flingue avant de le faire tomber au sol et partir. </p>
+    <p>“C..c’é..c’était qui ?”</p>
+    </p>
+    <button onclick="PageSuivante(2)">Page Précedente</button></div>
+    `,
+    `
+    <h3><strong>Chapitre 4: Aigle de sang</strong></h3>
+    <p>
+    Azrael a l’aube de son Apophis, si proche de la vengeance, arrivera dans la maison de son père par la porte d’entré entre ouverte, il poussera doucement la porte pour scruter l’intérieur, quelle ne fut pas sa surprise quand il vit les corps sans vie des domestiques et des gardes, une vraie boucherie digne d’un conte d’horreur, Azrael sous le feu de l’action entra dans le manoir et se diriga vers le bureau de son père pour avoir une chance de le tuer, mais en entrant il vit son père au sol, une balle dans la rotule se vidant de son sang, le jeune Garçon pistolet a la main prit peur qui a pu faire un telle chose se demanda le gamin avant que son père souffrant de douleur hurle…
+“Enflure, c'est ainsi que tu me remercies de t’avoir épargné ? ”dit t’il en se vidant de son sang…
+Azrael regarda son géniteur, Brandit le pistolet à silex de l'étrange BS pointa ce dernier sur son père, et brisa le silence glacial avec une détonation … 
+    </p>
+    <button onclick="PageSuivante(2)">Page Précedente</button></div>
+    `,
+    `
+    <h3><strong>Chapitre 5: Le Dechu</strong></h3>
+    <p>
+    Un équipement tridimensionnel !!! Pensa le gamin, il n’en a jamais utilisé, mais il savait que s'il n’improvisait pas, il était mort, sa seule option était la fuite avec cet équipement inconnue pour lui, mais cela étant décidé, il l’enfila et s’élança dans une course majestueuse qui se finit en collision avec un arbre, ce dernier tomba dans les vapes, mais fit récupérer par Zayn qui le traina plus loin.
+
+Azrael se réveilla chez lui, dans les bas-fonds, tout cela était donc un rêve, cela n’a-t-il jamais eu lieu, c'est ce qu’il pensait jusqu'à ce qu’il remarque l’équipement sur la table et sont amis Zayn en Stress…
+
+Après un soir mouvementé en explication, les 2 amis rejoignaient la pègre de shiganshina dirigé par Spade Yaxley dit L’as de pique dans le quelle ils deviendront 2 hommes d’élite du a leur maniement de l’équipement les fesant monter vite dans la hiérarchie criminelle
+Azrael et Zayn ont ainsi fait fortune et gloire dans les bas fonds, ou leur nom signifiât crainte à un point que même l’élite de la BS les craignait t’el le croc mitaine qu’ils étaient devenue, il vivait leur vie ainsi dans les bars et bordels de Spade.
+
+    </p>
+    <button onclick="PageSuivante(4)">Page Précedente</button></div>
+    `,
+    `
+    <h3><strong>Chapitre 6: Le bataillon D’exploration</strong></h3>
+    <p>
+    Durant leurs routine de voleur Azrael et Zayn se fit reperer par l'escouade du SGT Abarnikov,apres cela une poursuite au 3DM commencere,Au coin d’un carrefour, au croisement des chemins, lieu de pacte avec les démons, en plein dans les airs, Azrael et son compagnon Zayn pensait avoir échappé à leur poursuivant, il avait négligé un détail, des hommes du bataillon apparu de nulle part
+    comme tombé du ciel, avait 
+    suivit nos amis pour les 
+    menée dans un cul-de-sac ou
+    il avait été encerclé et attaqué
+    Zayn tomba assez vite et fit
+    neutralisé pendant que 
+    Azrael s’engagea dans un 
+    duel avec un des soldats du
+    bataillon capuche qui le neutr
+    -alisa a son tour, une fois les
+    menotte aux poignets et la chaine au coup, le CPL chef Raznov rejoint la festivité en ordonnant la mise à mort des 2 individus en murmurant j’avais tort a son ami SGT qui sourit…
+    <button onclick="PageSuivante(5)">Page Précedente</button></div>
+    `,
+    `
+    <h3><strong>Chapitre 8: Renaissance</strong></h3>
+    <p>
+    Le premier Jour a la surface sans être recherche leur semblait étrange, les autres soldats n’avait guère confiance en eux et les évitait, mais les 2 amis n'en avait rien à foutre, leur but comme dans les bas fond, la gloire, il excellait à l’équipement, mais il était froid et distant avec les autres, peux apprécier personne ne voulait d’eux, mais pas grave il comptait devenir des légendes.
+.
+    <p>
+
+    <button onclick="PageSuivante(6)">Page Précedente</button></div>
+    `,
+    `
+    <p>
+    les autres membres de l’escouade de tacticiens ne font qu’allouer des troupes a l’élimination de titans c’est stupide car c’est une expédition longue si on alloue nos troupe a l’élimination de titans dès le début on vas simplement épuisé nos troupe, c’est bien trop risqué, notre objectif doit être de trouvé un moyen d’éliminer les titans pas des les éliminer avec notre puissance militaire actuelle c’est impossible, mais impossible de les raisoner, la situation ne veut pas changer parceque 10 titans sont tomber on ne sais même pas comment il apparaisent par contre si 30 de nos hommes tombe la situation ne peux que changer, mais en tant que 1er C impossible de ce faire entendre. la situation m’énerve je souhaite que ma voix soit entendue au final, c’est comme lorsque j'étais à Utopia , c’est là que j’ai eu une idée je vais me battre rejoindre le commando en tant qu’aide temporaire le temps que la situation se calme, je remplacerais au moins un des leurs pour qu’il puissent parler de leur situation au tacticien, quelqu’un que j’ai connue et en qui j’ai confiance pour sa franchise et son honnêteté, mon niveau au tridimensionnel est certe médiocre mais si je doit faire ça pour être écouté alors je le ferais.
+    </p>
+
+    <button onclick="PageSuivante(7)">Page Précedente</button></div>
+    `,
+    `
+    `,
+];
+content_page2 = [
+    `<h3><strong>Identité</strong></h3><br><p>Nom: Gessner</p><p>Prenom: Konrad</p><br><p>Lieux de Naissance: Utopia</p><p>Date de Naissance: 13 Avril XXX</p><p>Age: 21 ans</p><br><p>Numéro D'identité: 4929 1049 6131 4085</p><p>Nombre de frère et soeurs: Fils Unique Orphelin</p><br><p>Lieux de Résidence: Trost</p><p>Evenement Notable:</p><dd> - N/A</dd><img style="width: auto; height: 30%;" src="public/img/Tampon-BS.jpg" alt=""><br><div class="flex-box"><button onclick="PageSuivante(2)">Page Suivante</button></div>`,
+    `
+    <h3><strong>Chapitre 2: évasion</strong></h3>
+    <p> Le jeune garçon, s'apprête à commettre l'impensable, rendre une visite de courtoisie à son géniteur responsable de tout son malheur, il sortira de cher lui vêtue d’une tunique pauvre avec une cape de Brigade Spécial anciennement “emprunter” a son paternel avec plein de trous dont se maudit logo a été rayé une lame dans la main gauche arborant un trou dans sa garde qui devait souvent arborer un joyau. 
+
+    Il se dirigea vers la sortie du “paradis” ou son paternelle l’avait bazardé, avec uniquement de mauvaises intentions, proche de la sortie il remarqua 2 brigades spéciales lui barrant la route, quand soudain un des 2 soldats tourna son regard vers le jeune garçon qui fonçait vers une mort imminente quand, avant le drame,
+    une main saisit le jeune le garçon et le traina dans une ruelle, une carrure grande et forte cachée sous une capuche se dévoile à lui, Zayn un ami et compagnon de fortune d’Azrael qui immédiatement lui dit :
+    </p>
+    <button onclick="PageSuivante(3)">Page Suivante</button></div>
+    `,
+    `
+    <p>
+    Un moment passa après cet événement qui ne fit qu'empirer la haine du jeune Azrael en vers son paternel, maintenant armé d’un pistolet et de conviction avec un plan de Zayn il s'apprête à sortir des bas fonds, ainsi un soir à l'aube de l'hiver le plus froid que connue shiganshina depuis des décennies, que fut commit le crime qui sera plus tard appelé le sang sur la Glace.
+
+Bref pour la faire courte ce soir Azrael réussi à sortir des bas fond, car la porte était bizarrement laissée sans surveillance de la BS, il en profita ainsi pour se faufiler en dehors des bas fonds, et se diriger vers la demeure du paternel pour “la visite de Courtoisie familial” qui tournera dans une tournure inattendu.
+
+    </p>
+    <button onclick="PageSuivante(4)">Page Suivante</button></div>
+    `,
+    `
+    <p>
+    Après le coup de feu, le placard du bureau s’ouvrit et 2 hommes vêtus de noir en sortirent, les 2 armés d’une épée et un pistolet à silex a 2 canons.
+
+“Je t’avais dit qu’il le ferait Guil tu me dois 400 royal marks le gamin à du cran ” dit le premier allant faire les poche de l’homme mort pendant que l’autre fixait le gamin avant de dire a son tour “certes il a du cran, mais il est trop impulsif il sera inutile à notre groupe Raz.” répliqua-t-il en ouvrant la fenêtre et s'apprêtent à partir, sur le départ l’homme masqué dénommé Raz dit au gamin “Barre-toi au plus vite, on t'a laissé un cadeau dans l’armoire à toi de bien l’utilisé”dit l’homme en quittant la pièce pour s’enfuir, à ce moment des hommes de la milice, on fait irruption dans le manoir, Azrael se dépêcha d’aller voir le mystérieux cadeau laissez par ces “nouveaux amis”, le gamin fit étonner par l’étrange cadeau…
+
+    </p>
+    <button onclick="PageSuivante(5)">Page Suivante</button></div>
+    `,
+    `
+    <p>ils étaient nourri, abreuvé et satisfait en femme, mais toute bonne chose comme la vie sont courtes, c'est ainsi que leur belle vie sera chamboulée par la grande rafle des bas fond de l’an 844.</p>
+    <p>
+    Dirige par le Caporal chef Abarnikov et le SGT Chef BDE Guilford pour rattraper la troupe de Spade munit d’un 3dmg, l’unité de la Rafle nomme la Leroy Groupe en hommage a un ancien Capitaine BS ont ainsi se jour la neutralisée plus de 80% de la pègre de Shiganshina, ainsi dans le feu de l’action les 2 hommes se pensant à l'abri dû à l’habitude du terrain et de la circonstance pensait pouvoir s’échappe de la situation, mais par malheur pour eux la routine est un fait qui les mène à commettre l’erreur fatale.
+
+    </p>
+    <button onclick="PageSuivante(6)">Page Suivante</button></div>
+    `,
+    `
+    <h3><strong>Chapitre 7:Mort</strong></h3>
+    <p>
+    À genoux devant les soldats, Azrael et son ami restaient fiers et droit attendant la mort, quand soudain, le SGT capuché c’était démasqué et dit.
+“Attendez, laissaient leur une chance de se racheté en rejoignant l’armé”
+Raznov se retourna et fixa le sergent, sortit deux pistolets, se retourna vers les jeunes garçons et les pointa, la seconde d’après 2 coups de feux retentir.
+Mais anormalement les 2 garçons était toujours en vie, l’homme qui voulait les tuer était debout avec un sourir, quand il prit la parole en leur disant une seule phrase.
+
+“Félicitation vous étés désoemmait mort, bienvenue dans votre nouvelle vie, maintenant vous allez faire ce que le SGT Guil vous demande.”
+
+    </p>
+    <br>
+    <button onclick="PageSuivante(7)">Page Suivante</button></div>
+    `,
+    `
+    `,
+];
+
+function PageSuivante(nb) {
+    document.getElementById("page1").innerHTML = content_page1[nb - 1];
+    document.getElementById("page2").innerHTML = content_page2[nb - 1];
+};
+
+setTimeout(function() {
+    PageSuivante(1);
+}, 10);
